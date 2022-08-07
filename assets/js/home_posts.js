cda10fe66a86edb044ae7b23661c4bad55764381
Samoresh Das <dsomu7@gmail.com>
2022-08-08 01:57:54 +0530

    task completed

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -15,6 +15,19 @@
                     let newPost= newPostDom(data.data.post);
                     $('#posts-list-container> ul').prepend(newPost);
                     deletePost($(' .delete-post-button',newPost));
+
+                    // call the create comment class
+                    new PostComments(data.data.post._id);
+
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post published!",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
+                    
                 },error: function(error){
                     console.log(error.responseText);
                 }
@@ -55,13 +68,21 @@
     //method to delete a post from DOM
     let deletePost= function(deleteLink){
         $(deleteLink).click(function(e){
-            e,preventDefault();
+            e.preventDefault();
 
             $.ajax({
                 type: 'get',
                 url: $(deleteLink).prop('href'),
                 success: function(data){
                     $(`#post-${data.data.post_id}`).remove();
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post Deleted",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
                 },error: function(error){
                     console.log(error.responseText);
                 }
@@ -71,5 +92,21 @@
 
 
 
+    // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+    let convertPostsToAjax = function(){
+        $('#posts-list-container>ul>li').each(function(){
+            let self = $(this);
+            let deleteButton = $(' .delete-post-button', self);
+            deletePost(deleteButton);
+
+            // get the post's id by splitting the id attribute
+            let postId = self.prop('id').split("-")[1]
+            new PostComments(postId);
+        });
+    }
+
+
+
     createPost();
+    convertPostsToAjax();
 }
--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -13,6 +13,19 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
+
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name');
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Comment created!"
+                });
+            }
+
             req.flash('success','Comment created');
             res.redirect('/');
             // return res.redirect('back');
@@ -32,6 +45,17 @@ module.exports.destroy= async function(req,res){
             let postId= comment.post;
             comment.remove();
             let post= await Post.findByIdAndUpdate(postId,{$pull:{comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Comment deleted"
+                });
+            }
+
             req.flash('success','Comment deleted');
             return res.redirect('back');
         }else{
